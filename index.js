da88d67947662ff31718f176415bf234804c5118
asyraf07 <asyraf.sr07@gmail.com>
2022-01-30 09:12:30 +0700

    add readme.md

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 /* app.use(express.static(path.join(__dirname + "client/build"))); */
-/* app.use(express.static("./client/build")); */
+app.use(express.static("./client/build"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("./client/build"));
